4fdb3c788f40f8b2dfb4d29ee3dc09cc59d8321f
anshu <manu22425@gmail.com>
2024-03-31 15:32:04 +0530

    Unneccessary code removal

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const connectToMongo = require('./db');
 const bodyParser = require('body-parser');
-
-const path = require('path')
 connectToMongo();
 const app = express();
 const authRoutes = require('./routes/authRoutes');
 const notesRoutes = require('./routes/notesRoutes');
 app.use(bodyParser.json());
-app.get('/', (req, res) => {
-   res.send("Hello world");
-});
+
 
 app.use('/auth', authRoutes);
 app.use('/api/notes', notesRoutes)
